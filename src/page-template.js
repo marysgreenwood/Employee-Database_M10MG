44f9b8d3212ae77748f2ffb18dcef2342cc10e50
marysgreenwood <marysgreenwood@gmail.com>
2023-03-11 15:18:08 -0500

    added page template

--- src/page-template.js
+++ src/page-template.js
@@ -1,9 +1,8 @@
 // create the team
-const generateTeam = team => {
-
-    // create the manager html
-    const generateManager = manager => {
-        return `
+const generateTeam = (team) => {
+  // create the manager html
+  const generateManager = (manager) => {
+    return `
         <div class="card employee-card">
         <div class="card-header">
             <h2 class="card-title">${manager.getName()}</h2>
@@ -18,43 +17,72 @@ const generateTeam = team => {
         </div>
     </div>
         `;
-    };
+  };
 
-    // create the html for engineers
-    const generateEngineer = engineer => {
-        //code goes here
-    };
+  // create the html for engineers
+  const generateEngineer = (engineer) => {
+    return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${engineer.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${engineer.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${engineer.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li class="list-group-item">Github: <a href= "https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></li>
+            </ul>
+        </div>
+    </div>
+        `;
+  };
 
-    // create the html for interns
-    const generateIntern = intern => {
-        //code goes here
-    };
+  // create the html for interns
+  const generateIntern = (intern) => {
+    return `
+        <div class="card employee-card">
+        <div class="card-header">
+            <h2 class="card-title">${intern.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${intern.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${intern.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                <li class="list-group-item">School:${intern.getSchool()}</li>
+            </ul>
+        </div>
+    </div>
+        `;
+  };
 
-    const html = [];
+  const html = [];
 
-    html.push(team
-        .filter(employee => employee.getRole() === "Manager")
-        .map(manager => generateManager(manager))
-    );
-    html.push(team
-        .filter(employee => employee.getRole() === "Engineer")
-        .map(engineer => generateEngineer(engineer))
-        .join("")
-    );
-    html.push(team
-        .filter(employee => employee.getRole() === "Intern")
-        .map(intern => generateIntern(intern))
-        .join("")
-    );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => generateManager(manager))
+  );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => generateEngineer(engineer))
+      .join("")
+  );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => generateIntern(intern))
+      .join("")
+  );
 
-    return html.join("");
-
-}
+  return html.join("");
+};
 
 // export function to generate entire page
-module.exports = team => {
-
-    return `
+module.exports = (team) => {
+  return `
     <!DOCTYPE html>
 <html lang="en">
 
